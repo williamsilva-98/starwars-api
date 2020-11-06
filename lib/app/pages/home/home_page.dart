04cc972ce626b9f79a55abc81d7c7d699946185f
William Silva <williamhenrique.silva98@gmail.com>
2020-11-06 19:55:22 -0300

    Diagramação HomePage e adição de assets

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -1,10 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Image.asset(
+          'assets/images/star_wars_logo.png',
+          height: 50,
+        ),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(16, 32, 16, 16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              'Conheça os principais detalhes dos personagens de StarWars.',
+              style: TextStyle(
+                fontSize: 22,
+                color: Colors.black.withOpacity(0.8),
+              ),
+              textAlign: TextAlign.start,
+            ),
+            SizedBox(height: 32),
+            TextFormField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Nome do personagem',
+              ),
+            ),
+            SizedBox(height: 32),
+            Container(
+              height: 50,
+              decoration: BoxDecoration(
+                color: Get.theme.accentColor,
+                borderRadius: BorderRadius.circular(8),
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(1, 1),
+                    blurRadius: 1,
+                    color: Colors.black.withOpacity(0.3),
+                  ),
+                ],
+              ),
+              child: Center(
+                child: Text(
+                  'VER DETALHES',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
