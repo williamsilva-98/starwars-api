4a9d4adf04536fc32dce1476cb200d72a767567b
William Silva <williamhenrique.silva98@gmail.com>
2020-11-06 19:28:37 -0300

    Configuração do tema da aplicação

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:swapi_app/app/pages/home/home_page.dart';
+import 'package:swapi_app/app/theme/app_theme.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.appTheme,
       home: HomePage(),
     );
   }
---
+++ lib/app/theme/app_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final ThemeData appTheme = ThemeData(
+    primaryColor: Colors.black,
+    accentColor: Color(0xFFFFE81F),
+  );
+}
